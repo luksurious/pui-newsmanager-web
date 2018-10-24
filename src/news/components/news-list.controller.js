7029f137644723fe1f59876c8c7f4cd87e86424c
Lukas Brückner <me@luksurious.me>
2018-10-24 19:24:32 +0200

    add some informational headers

--- src/news/components/news-list.controller.js
+++ src/news/components/news-list.controller.js
@@ -2,19 +2,23 @@ angular.module('news.app')
     .controller('NewsListController', function ($scope, $routeParams, NewsListService) {
         $scope.data = [];
 
-        $scope.loaded = false;
+        $scope.isLoaded = false;
+        $scope.isSearch = false;
+        $scope.isCategoryView = false;
         
         $scope.newsFilter = {};
         if ($routeParams.category) {
             $scope.newsFilter.category = $routeParams.category;
+            $scope.isCategoryView = true;
         }
         if ($routeParams.term) {
             $scope.newsFilter = $routeParams.term;
+            $scope.isSearch = true;
         }
 
         NewsListService.query().$promise.then(data => {
             $scope.data = data;
-            $scope.loaded = true;
+            $scope.isLoaded = true;
         }).catch(e => {
             console.log(e);
         });
