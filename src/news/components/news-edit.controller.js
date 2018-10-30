020e48b5711eeeed63443ffd647419fbea29c447
Vincent <giardina.v@gmail.com>
2018-10-30 19:23:19 +0100

    Edit good

--- src/news/components/news-edit.controller.js
+++ src/news/components/news-edit.controller.js
@@ -1,15 +1,23 @@
 angular.module('news.app')
-    .controller('NewsEditController', function ($scope, NewsDetailsService, $routeParams) {
+    .controller('NewsEditController', function ($scope, NewsDetailsService, ngToast, $routeParams) {
         $scope.data = {};
-        
+
         $scope.newsId = $routeParams.id;
-        
+
         NewsDetailsService.get({id: $scope.newsId}).$promise.then(data => {
             $scope.data = data;
         })
         .catch(e => {
             console.log(e);
         });
+
+        $scope.save = function(){
+            NewsDetailsService.save($scope.data).$promise.then(
+                function(){
+                    ngToast.create('Saved !');
+                }
+            )
+        }
     });
 
 // TODO: manage authentification
